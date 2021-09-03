83ed102548d55ef1cf09424b76e3dc364b00b50a
IGamesI <69199901+IGamesI@users.noreply.github.com>
2021-09-03 19:12:06 +0430

    Added Pages

---
+++ CourseBox.ReactNative/pages/search.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyleSheet, View, Text } from 'react-native';
+import { golbalStyles } from "../shared/globalStyle";
+
+export default function Search() {
+    return (
+        <View>
+            <Text>Profile Screen</Text>
+        </View>
+    )
+}
