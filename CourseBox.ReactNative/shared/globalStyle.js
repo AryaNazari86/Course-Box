9c23acd472630d8645e7ccdc98bb3de347e82309
mohammad488 <mhk488@outlook.com>
2021-09-05 08:19:42 +0430

    Heder Deleted

--- CourseBox.ReactNative/shared/globalStyle.js
+++ CourseBox.ReactNative/shared/globalStyle.js
@@ -1,26 +1,5 @@
 import { StyleSheet, Dimensions } from "react-native";
 export const globalStyles = StyleSheet.create({
-    headerContainer: {
-        marginBottom: 10,
-        backgroundColor: '#E5E5E5',
-    },
-    headerDetails: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: 70,
-    },
-    headerText: {
-        top: 10,
-        fontSize: 25,
-        fontWeight: 'bold',
-        color: '#14213D',
-        textAlign: 'center',
-        marginTop: 35,
-    },
-    headerSvgCurve: {
-        position: 'absolute',
-        width: Dimensions.get('window').width,
-    },
     tabBar: {
         backgroundColor: '#14213D',
     },
