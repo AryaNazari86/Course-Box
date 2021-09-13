81a62d76a0b8bd3b8eb409ca6dfb816642f64d12
AryaNazari86 <nazariarya86@gmail.com>
2021-09-14 00:12:42 +0430

    added more styles
    added more styles

--- CourseBox.ReactNative/pages/coursePreview.js
+++ CourseBox.ReactNative/pages/coursePreview.js
@@ -1,44 +1,48 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import Header from '../shared/header';
 
 export default function CoursePreview(props) {
     const course = props.navigation.state.params.item;
     return (
-        <View>
+        <View style={{ backgroundColor: '#fca311', flex: 1 }}>
             <Header title={course.title} backButton={true} backAction={() => props.navigation.goBack()} fontFamily="rubik-regular" height={60} />
-            {course.content.map(
-                (item) => {
-                    return (
-                        <View style={styles.subject}>
-                            {/* Subject's title */}
-                            <View style={{ ...styles.subjectTitle, borderColor: course.color }}>
-                                {/* Icon */}
-                                <MaterialIcons name={item.icon} />
-                                {/* Text */}
-                                <Text style={{ ...styles.subjectTitleText, color: course.color }}>{item.title}</Text>
-                            </View>
-                            {/* Lessons of the subject  */}
-                            <View style={styles.subjectLessons}>
-                                {item.content.map(
-                                    (item) => {
-                                        {/* Each lesson of the subject  */ }
-                                        return (
-                                            <TouchableOpacity style={{ ...styles.lesson, borderColor: course.color }}>
-                                                {/* lesson's icon */}
-                                                <MaterialIcons name={item.icon} size={50} color={course.color} style={styles.lessonIcon} />
-                                                {/* lesson's title */}
-                                                <Text style={{ ...styles.lessonTitleText, color: course.color }}>{item.title}</Text>
-                                            </TouchableOpacity>
-                                        )
-                                    }
-                                )}
-                            </View>
-                        </View >
-                    )
-                }
-            )}
+            <View >
+                {course.content.map(
+                    (item) => {
+                        return (
+                            <View style={styles.subject}>
+                                {/* Subject's title */}
+                                <View style={{ ...styles.subjectTitle, borderColor: course.color }}>
+                                    {/* Icon */}
+                                    <MaterialIcons name={item.icon} size={100} color={course.color} style={styles.subjectIcon} />
+                                    {/* Text */}
+                                    <Text style={{ ...styles.subjectTitleText, color: course.color }}>{item.title}</Text>
+                                </View>
+                                {/* Lessons of the subject  */}
+                                <View style={styles.subjectQls}>
+                                    {item.content.map(
+                                        (item) => {
+                                            {/* Each lesson of the subject  */ }
+                                            return (
+                                                <TouchableOpacity style={{ ...styles.ql, borderColor: course.color }}>
+                                                    {/* lesson's icon */}
+                                                    <MaterialIcons name={item.icon} size={50} color={course.color} style={{ ...styles.qlIcon, borderColor: course.color }} />
+                                                    {/* lesson's title */}
+                                                    <Text style={{ ...styles.qlTitleText, color: course.color }}>{item.title}</Text>
+                                                </TouchableOpacity>
+                                            )
+                                        }
+                                    )}
+                                </View>
+
+                            </View >
+                        )
+                    }
+
+                )}
+            </View>
         </View >
     )
 }
@@ -48,24 +52,24 @@ const styles = StyleSheet.create({
         marginVertical: 15,
         marginHorizontal: 30,
     },
+    subjectIcon: {
+        alignSelf: 'center',
+    },
     subjectTitle: {
         alignSelf: 'center',
-        borderWidth: 1,
-        borderRadius: 20,
 
     },
     subjectTitleText: {
         alignSelf: 'center',
         fontSize: 25,
     },
-    subjectLessons: {
+    subjectQls: {
         flexDirection: 'row',
+        justifyContent: 'center',
     },
     ql: {
-        borderWidth: 1,
         marginHorizontal: 5,
         marginTop: 10,
-        borderRadius: 20,
         height: 100,
         width: 100,
     },
@@ -75,6 +79,9 @@ const styles = StyleSheet.create({
         top: 20,
     },
     qlIcon: {
+        padding: 10,
+        borderWidth: 5,
+        borderRadius: 40,
         alignSelf: 'center',
     }
 })
