6a3ed4d5ac9a98257ffa0fd29db3ad37f5c059b6
mohammad488 <mhk488@outlook.com>
2021-09-30 21:42:38 +0330

    User Model

--- CourseBox.Flask/app.py
+++ CourseBox.Flask/app.py
@@ -1,42 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_user import UserManager, UserMixin
 
 app = Flask(__name__)
-db = SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///coursebox.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
-# Define User data-model
-class User(db.Model, UserMixin):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-
-    # User Authentication fields
-    #email = db.Column(db.String(255), nullable=False, unique=True)
+class User(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True, unique=True)
     username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(200), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
+    password_salt = db.Column(db.String, nullable=False)
+    active_code = db.Column(db.String(10), nullable=False)
+    is_active = db.Column(db.Boolean, nullable=False)
+    avatar = db.Column(db.String(200))
+    register_date = db.Column(db.DateTime, nullable=False)
 
-    # User fields
-    active = db.Column(db.Boolean()),
-    '''first_name = db.Column(db.String(50), nullable=False)
-    last_name = db.Column(db.String(50), nullable=False)'''
-    def __init__(self, username, password):
+    def __init__(self, username, email, password, password_salt, active_code, is_active, avatar, register_date):
         self.username = username
+        self.email = email
+        self.email = email
         self.password = password
-# Setup Flask-User
-user_manager = UserManager(app, db, User)
+        self.password_salt = password_salt
+        self.active_code = active_code
+        self.is_active = is_active
+        self.avatar = avatar
+        self.register_date = register_date
 
-
-
-@app.route("/", method=['POST', 'GEST'])
-def user():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = User(username, password)
-        db.session.add(user)
-        db.session.commit()
-        return 'Ilia'
-    return render_template('index.html')
 
 if __name__ == '__main__':
     db.create_all()
