4f3a3d92c2eefe843a6a6275f3ef7445ee88f5f5
IGamesI <69199901+IGamesI@users.noreply.github.com>
2021-10-06 08:32:51 +0330

    Save avatar name to database

--- CourseBox.Flask/app.py
+++ CourseBox.Flask/app.py
@@ -28,10 +28,12 @@ class Course(db.Model):
         self.category_id = category_id
         self.author_id = author_id
 
+
 class CourseParticipant(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True)
     course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
     participant_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True)
@@ -39,10 +41,11 @@ class Category(db.Model):
     category_image = db.Column(db.String(200))
     # Relations
     courses = db.relationship('Course', backref='category')
-  
+
     def __init__(self, title, category_image):
         self.title = title
         self.category_image = category_image
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True)
@@ -71,8 +74,8 @@ class User(db.Model):
 
 
 # Change the profile avatar
-@app.route("/ChangeProfileAvatar", methods=["POST"])
-def Change_Profile_Avatar():
+@app.route("/ChangeProfileAvatar/<user_id>", methods=["POST"])
+def Change_Profile_Avatar(user_id):
     avatar = request.files['avatar']
     if avatar == None:
         status_code = Response(status=400, response="")
@@ -80,14 +83,21 @@ def Change_Profile_Avatar():
     allowed_extensions = [".png", ".jpeg", ".jpg", ".webp", ".ico", ".svg"]
     file_extension = os.path.splitext(avatar.filename)[-1]
     if allowed_extensions.__contains__(file_extension):
+        avatar_uid = uuid.uuid4()
         avatar.save(os.path.join("avatars", str(
-            uuid.uuid4()) + file_extension))
+            avatar_uid) + file_extension))
+
+        user_id = User.query.filter_by(id=user_id).first()
+        user_id.avatar = str(avatar_uid) + file_extension
+        db.session.commit()
+
         status_code = Response(status=200, response="")
         return status_code
 
     else:
         status_code = Response(status=400, response="")
         return status_code
+
 
 if __name__ == '__main__':
     db.create_all()
