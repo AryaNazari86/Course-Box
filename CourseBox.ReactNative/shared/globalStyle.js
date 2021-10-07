df6994975d59c7205bcb9a3882130f4e704304cd
IGamesI <69199901+IGamesI@users.noreply.github.com>
2021-10-07 08:36:05 +0330

    Added white texts and comments to globalStyle

--- CourseBox.ReactNative/shared/globalStyle.js
+++ CourseBox.ReactNative/shared/globalStyle.js
@@ -1,67 +1,110 @@
 import { StyleSheet } from "react-native";
 export const globalStyles = StyleSheet.create({
+  // Container of the page
   container: {
     alignItems: "center",
     backgroundColor: "#EDF2F4",
   },
+
+  // Header
   header: {
     elevation: 0,
     backgroundColor: "#1D3557",
   },
+
+  // Title of the header
   headerTitle: {
     fontSize: 26,
     color: "#A8DADC",
     paddingBottom: 7,
   },
+
+  // Header back button
   headerBackAnimation: {
     width: 50,
     height: 50,
   },
+
+  // Tabbar
   tabBar: {
     height: 70,
     backgroundColor: "#1D3557",
   },
+
+  // Used for react-native-paper inputs
   input: {
     marginTop: 35,
     marginBottom: 15,
     width: "85%",
     height: 50,
   },
+
+  // Used for Titles
   TitleText: {
     fontSize: 24,
   },
+
+  // Used for Titles on a black background
+  whiteTitleText: {
+    fontSize: 24,
+    color: "#fff",
+  },
+
+  // Used for spacing components (Used a lot when scrollbar doesn't scroll to the bottom)
   emptySpacer: {
     marginTop: 75,
     opacity: 0,
   },
+
+  // Used for highlited texts or links
   highlitedText: {
     color: "#399DFF",
   },
+
+  // Used for showing errors
   errorText: {
     color: "crimson",
     fontWeight: "bold",
     marginTop: 6,
     textAlign: "center",
   },
+
+  // Used for Normal texts
   normalText: {
     flexDirection: "row",
     fontFamily: "rubik-light",
     paddingTop: 20,
   },
+
+  // Used for Normal texts on a black background
+  normalWhiteText: {
+    flexDirection: "row",
+    fontFamily: "rubik-light",
+    paddingTop: 20,
+    color: "#fff",
+  },
+
+  // Text of the button
   buttonText: {
     fontSize: 25,
     color: "white",
     fontFamily: "rubik-bold",
   },
+
+  // Container of loading animation
   loaderContainer: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
+
+  // Style of loading animation
   loader: {
     width: 200,
     height: 200,
   },
+
+  // Style of the button
   button: {
     backgroundColor: "#EF233C",
     width: 330,
@@ -72,7 +115,8 @@ export const globalStyles = StyleSheet.create({
     marginTop: 20,
     marginBottom: 20,
   },
-  // The text input
+
+  // Style for the default react native text input component
   inputComp: {
     paddingHorizontal: 8,
     paddingVertical: 6,
@@ -81,6 +125,7 @@ export const globalStyles = StyleSheet.create({
     fontSize: 18,
     fontFamily: "rubik-regular",
   },
+
   // The view covering the text input
   textInputView: {
     borderWidth: 1,
@@ -92,9 +137,13 @@ export const globalStyles = StyleSheet.create({
     backgroundColor: "#A8DADC",
     marginTop: 20,
   },
+
+  // Style of the button that hides password
   hideIcon: {
     padding: 25,
   },
+
+  // Used for showing two texts (mostly a normalText and a highlightText) beside each other
   normalAndHighlightContainer: {
     flexDirection: "row",
     fontFamily: "rubik-light",
