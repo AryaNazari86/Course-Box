5d0322b4deedc3959b339dbda5de0a7e3c7fe309
IGamesI <69199901+IGamesI@users.noreply.github.com>
2021-11-27 20:50:46 +0330

    Added animated icons to project code

--- CourseBox.ReactNative/components/CourseBox/courseBoxIcon.js
+++ CourseBox.ReactNative/components/CourseBox/courseBoxIcon.js
@@ -1,44 +1,40 @@
-import { Image } from 'react-native';
-import React from 'react';
-export default function CategoryIcon({category, style}) {
-    if (category == 'Sports') {
-        return (
-            <Image
-                source={require('../../assets/3DIcons/رقص.gif')}
-                style={style}
-            />
-        );
-    }
-    else if (category == 'Maths') {
-        return (
-            <Image
-                source={require('../../assets/3DIcons/رقص.gif')}
-                style={style}
-            />
-        );
-    }
-    else if (category == 'Programming') {
-        return (
-            <Image
-                source={require('../../assets/3DIcons/Code_3DIcon.png')}
-                style={style}
-            />
-        );
-    }
-    else if (category == 'Art') {
-        return (
-            <Image
-                source={require('../../assets/3DIcons/Paint_3DIcon.png')}
-                style={style}
-            />
-        );
-    }
-    else if (category == 'Science') {
-        return (
-            <Image
-                source={require('../../assets/3DIcons/Science_3DIcon.png')}
-                style={style}
-            />
-        );
-    }
+import { Image } from "react-native";
+import React from "react";
+export default function CategoryIcon({ category, style }) {
+  if (category == "Sports") {
+    return (
+      <Image
+        source={require("../../assets/3DIcons/Animated/Sports.gif")}
+        style={style}
+      />
+    );
+  } else if (category == "Maths") {
+    return (
+      <Image
+        source={require("../../assets/3DIcons/Animated/Math.gif")}
+        style={style}
+      />
+    );
+  } else if (category == "Programming") {
+    return (
+      <Image
+        source={require("../../assets/3DIcons/Animated/Computer.gif")}
+        style={style}
+      />
+    );
+  } else if (category == "Art") {
+    return (
+      <Image
+        source={require("../../assets/3DIcons/Animated/Paint.gif")}
+        style={style}
+      />
+    );
+  } else if (category == "Science") {
+    return (
+      <Image
+        source={require("../../assets/3DIcons/Animated/Science.gif")}
+        style={style}
+      />
+    );
+  }
 }
