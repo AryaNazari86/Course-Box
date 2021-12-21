bdb804560a1164d1ea8cd3f1170dcf44eb02db37
AryaNazari86 <nazariarya86@gmail.com>
2021-12-22 01:03:04 +0330

    Lesson Block API function in front end

--- CourseBox.ReactNative/Services/courseService.js
+++ CourseBox.ReactNative/Services/courseService.js
@@ -1,7 +1,7 @@
 import React from "react";
 import md5 from 'md5';
 
-const API_ADDRESS = "192.168.6.8";
+const API_ADDRESS = "192.168.6.8:5000";
 
 export async function GetLatestCourses() {
     try {
@@ -10,13 +10,46 @@ export async function GetLatestCourses() {
             response: "",
             data: ""
         };
-        await fetch('http://' + API_ADDRESS + ':5000/LatestCourses', { method: 'GET', headers: { 'Content-Type': 'application/json' } })
+        await fetch('http://' + API_ADDRESS + '/LatestCourses', { method: 'GET', headers: { 'Content-Type': 'application/json' } })
             .then(response => {
                 if (response.status == 200) {
                     result.successful = true;
                     result.data = response.json();
                 }
                 else {
+                    result.response = "Error...";
+                }
+            });
+        return result;
+    } catch (error) {
+        return {
+            successful: false,
+            response: "Error..."
+        };
+    }
+}
+
+export async function AddLessonBlock(values) {
+    try {
+        let block = {
+            type: values.type,
+            content: values.content,
+            lesson_id: values.lesson_id,
+        };
+        let result = {
+            successful: false,
+            response: ""
+        };
+        await fetch('http://' + API_ADDRESS + '/Course/AddLessonBlock', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(block) })
+            .then(response => {
+                result.response = response.statusText;
+                if (response.status == 200) {
+                    result.successful = true;
+                }
+                else if (response.status == 500) {
+                    result.response = "Error...";
+                }
+                else if (response.status == 404){
                     result.response = "Error...";
                 }
             });
