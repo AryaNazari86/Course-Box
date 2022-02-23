70f347abea8e7453e13e2a5203af35bb373ab662
Mohammad Hajikandi <73636328+mohammad488@users.noreply.github.com>
2022-02-23 09:26:01 +0330

    Update courseBoxIcon.js

--- CourseBox.ReactNative/components/CourseBox/courseBoxIcon.js
+++ CourseBox.ReactNative/components/CourseBox/courseBoxIcon.js
@@ -4,12 +4,16 @@ import { GetCategory } from "../../Services/courseService";
 import { useState } from "react";
 export default function CategoryIcon({ category, style }) {
   const [categoryImage, setCategoryImage] = useState(
-    "../../assets/3DIcons/Animated/Soccer.gif"
+    reguire("../../assets/3DIcons/Animated/Soccer.gif")
   );
-  GetCategory().then((result) => {
-    setCategoryImage(
-      "../../assets/3DIcons/Animated/" + result.data.category_image
-    );
-  });
+  const [categoryFetched, setCategoryFetched] = useState(false);
+  if(!categoryFetched){
+    GetCategory().then((result) => {
+      setCategoryImage(
+        require("../../assets/3DIcons/Animated/" + result.data.category_image)
+      );
+    });
+    setCategoryFetched(true);
+  }
   return <Image source={categoryImage} style={style} />;
 }
